82cbc56354fe4a2f49f42014b3de0dbfd7bb2860
Suraj-Sangwan <root@localhost.localdomain>
2025-05-25 02:52:47 +0000

    Welcome to EvolutionX

--- lineage_fajita.mk
+++ lineage_fajita.mk
@@ -24,6 +24,11 @@ $(call inherit-product, device/oneplus/fajita/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+#FLAGS
+WITH_GMS := true
+TARGET_USES_MINI_GAPPS := true
+TARGET_HAS_UDFPS := true
+
 PRODUCT_NAME := lineage_fajita
 PRODUCT_DEVICE := fajita
 PRODUCT_MANUFACTURER := OnePlus
