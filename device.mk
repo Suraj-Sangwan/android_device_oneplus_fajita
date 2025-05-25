75eebdd0b53a86ce8ac7d41abf2a2fa8815f5c79
Suraj-Sangwan <root@localhost.localdomain>
2025-05-25 02:52:47 +0000

    Welcome to EvolutionX

--- device.mk
+++ device.mk
@@ -27,6 +27,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-evolution \
     $(LOCAL_PATH)/overlay-lineage
 
 # Device uses high-density artwork where available
